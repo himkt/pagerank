37d4f1f40888479d68cc460994d4ae7f2bd487cf
himkt <himkt@klis.tsukuba.ac.jp>
2015-06-10 22:55:37 +0900

    add: coverall configurations

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,4 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pagerank'
-require 'coveralls'
-Coveralls.wear!
+
+if ENV['CI']
+  require 'coveralls'
+  Coveralls.wear!
+
+  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[Coveralls::SimpleCov::Formatter]
+  SimpleCov.start 'test_frameworks'
+end
