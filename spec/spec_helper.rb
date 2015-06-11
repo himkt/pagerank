1a34789cbdf954cb8148cffab56db1b47c94d5e1
himkt <himkt@klis.tsukuba.ac.jp>
2015-06-11 13:25:14 +0900

    remove: coveralls test

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,3 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'pagerank'
 
-if ENV['CI']
-  require 'coveralls'
-  Coveralls.wear!
-
-  SimpleCov.formatter = SimpleCov::Formatter::MultiFormatter[Coveralls::SimpleCov::Formatter]
-  SimpleCov.start 'test_frameworks'
-end
